77c37b4ae75a1dbcd9874dae05b054a00cbc9d2d
PretoriusMikael <mikaelpretorius@gmail.com>
2024-09-02 18:46:00 +0200

    Added footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <div class="contact">
+          <h3 id="Contact Details">Contact Details:</h3>
+          <ul>
+            <li>
+              <b>Full name: </b>Mikael Simeon Pretorius
+            </li>
+            <li>
+              <b>Cellphone: </b>081 382 7201
+            </li>
+            <li>
+              <b>Email address: </b>mikaelpretorius@gmail.com
+            </li>
+            <li>
+              <b>Social: </b>
+              <a target="_blank" href="https://instagram.com/mikael_pretorius">
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
